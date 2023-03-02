f823841283f4cc288b389a5a4fdcd42056b317af
mahakchaich <mahakchaich105@gmail.com>
2023-03-02 14:37:26 +0100

    fixed2 commit

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,14 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CommandeController;
-use App\Http\Controllers\CommandePanierController;
-use App\Http\Controllers\PanierController;
-use App\Http\Controllers\UtilisateurController;
 use App\Models\Utilisateur;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PanierController;
+use App\Http\Controllers\CommandeController;
+use App\Http\Controllers\UtilisateurController;
+use App\Http\Controllers\CommandePanierController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,27 +37,12 @@ function common(string $scope)
 
 //admin
 Route::prefix('admin')->group(function () {
-<<<<<<< HEAD
     common('scope.admin');
     Route::middleware(['auth:sanctum', 'scope.admin'])->group(function () {
         //User Management
-        Route::get('users', [UtilisateurController::class, 'getusers']);
-        Route::get('user/{id}', [UtilisateurController::class, 'getUserById']);
-        Route::post('adduser', [UtilisateurController::class, 'addUser']);
-        Route::put('updateuser/{id}', [UtilisateurController::class, 'updateUser']);
-        Route::delete('deleteuser/{id}', [UtilisateurController::class, 'deleteUser']);
-=======
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
-
-
-    Route::middleware(['auth:sanctum', 'scope.admin'])->group(function () {
-        Route::get('user', [AuthController::class, 'user']);
-        Route::post('logout', [AuthController::class, 'logout']);
-        Route::put('users/info', [AuthController::class, 'updateInfo']);
-        Route::put('users/password', [AuthController::class, 'updatePassword']);
-        Route::get('utilisateur', [UtilisateurController::class, 'index']);
->>>>>>> a165a3fa0ca3e0f898182721987280afb2b8c717
+        Route::apiResource('users', UserController::class);
+        //Panier Management
+        Route::apiResource('paniers', PanierController::class);
     });
 });
 
@@ -74,8 +60,7 @@ Route::prefix('user')->group(function () {
 
 
 
-//Panier
-Route::apiResource('paniers', PanierController::class);
+
 
 //Commande
 Route::get('commandes', [CommandeController::class, 'index']);
